cf9d3b256e37bbfe79d710c76ef9ee4e880f0815
bdebatata <bdebatatadoce@gmail.com>
2025-06-10 14:30:20 -0300

    Refatoração da função que valida linha para manter um padrão

--- velha.cpp
+++ velha.cpp
@@ -1,15 +1,11 @@
 #include "velha.hpp"
 #include <vector>
 using namespace std;
-bool valida_linha_X(vector<int> linha){
-    int tamanhoLinha = linha.size();
-    int qtdUns = 0;
-    while(tamanhoLinha--){
-        if(linha[tamanhoLinha] == 1){
-            qtdUns++;
-        }
-    }
-    return qtdUns == 3;
+bool valida_linha_X(vector<vector<int>> tabuleiro){
+    bool primeiraLinha = (tabuleiro[0][0] == 1)&&(tabuleiro[0][1] == 1)&&(tabuleiro[0][2] == 1);
+    bool segundaLinha = (tabuleiro[1][0] == 1)&&(tabuleiro[1][1] == 1)&&(tabuleiro[1][2] == 1);
+    bool terceiraLinha = (tabuleiro[2][0] == 1)&&(tabuleiro[2][1] == 1)&&(tabuleiro[2][2] == 1);
+    return primeiraLinha||segundaLinha||terceiraLinha;
 }
 bool valida_coluna_X(vector<vector<int>> tabuleiro){
     bool primeiraColuna = (tabuleiro[0][0] == 1)&&(tabuleiro[1][0] == 1)&&(tabuleiro[2][0] == 1);
@@ -19,7 +15,7 @@ bool valida_coluna_X(vector<vector<int>> tabuleiro){
 }
 int verifica_resultado(const vector<vector<int>> tabuleiro)
 {
-    bool linhaDeX = valida_linha_X(tabuleiro[0]) || valida_linha_X(tabuleiro[1]) || valida_linha_X(tabuleiro[2]);
+    bool linhaDeX = valida_linha_X(tabuleiro);
     bool colunaDeX = valida_coluna_X(tabuleiro);
     if(linhaDeX||colunaDeX){
         return 1;
