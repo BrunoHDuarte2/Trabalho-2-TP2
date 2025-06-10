a05dbce493474c96b1850a6b53cb5cfa2a5046be
bdebatata <bdebatatadoce@gmail.com>
2025-06-10 14:26:33 -0300

    Teste passa para validação de colunas de X

--- velha.cpp
+++ velha.cpp
@@ -11,10 +11,17 @@ bool valida_linha_X(vector<int> linha){
     }
     return qtdUns == 3;
 }
+bool valida_coluna_X(vector<vector<int>> tabuleiro){
+    bool primeiraColuna = (tabuleiro[0][0] == 1)&&(tabuleiro[1][0] == 1)&&(tabuleiro[2][0] == 1);
+    bool segundaColuna = (tabuleiro[0][1] == 1)&&(tabuleiro[1][1] == 1)&&(tabuleiro[2][1] == 1);
+    bool terceiraColuna = (tabuleiro[0][2] == 1)&&(tabuleiro[1][2] == 1)&&(tabuleiro[2][2] == 1);
+    return primeiraColuna||segundaColuna||terceiraColuna;
+}
 int verifica_resultado(const vector<vector<int>> tabuleiro)
 {
     bool linhaDeX = valida_linha_X(tabuleiro[0]) || valida_linha_X(tabuleiro[1]) || valida_linha_X(tabuleiro[2]);
-    if(linhaDeX){
+    bool colunaDeX = valida_coluna_X(tabuleiro);
+    if(linhaDeX||colunaDeX){
         return 1;
     }
     return 0;
